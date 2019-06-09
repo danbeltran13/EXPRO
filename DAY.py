a1070209d27702aaf0f69da7dce3350dba5d2bac
danbeltran13 <danielbeltran1303@gmail.com>
2019-06-09 18:59:46 -0500

    Current Files
    These are the current files for the project

---
+++ DAY.py
@@ -0,0 +1,170 @@
+
+
+# TODO: ADD FEATURES TO PROGRAM - WORK ON DISPLAYING WEIGHT, TIME, BD
+# TODO: FIND MAX OF THE EXCERSISE, VOLUME
+
+# total volume for excersice, volume for a set, max of excercise,
+
+
+
+import tkinter
+import matplotlib
+import pro as list
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import matplotlib.dates as dates
+
+
+class board:
+    # WILL DISPLAY GRAPH BASED ON VALUES GIVEN FROM LISTBOXES
+    def drawGraph(self, box_x,box_y):
+        # GETS SELECTED VALUES OF THE LISTBOXES
+        x = box_x.get(tkinter.ACTIVE)
+        y = box_y.get(tkinter.ACTIVE)
+
+        #GETS THE X AND Y VALUES
+        x_values = list.returnList(x)
+        y_values = list.returnList(y)
+        # FIXES THE LENGHT OF THE LISTS
+        list.fixLenght(x_values,y_values)
+
+        fig = Figure(figsize=(9,4))
+        a = fig.add_subplot(111)
+
+        print(x_values)
+        print(y_values)
+        if x == 'DATES':
+            a.plot_date(dates.datestr2num(x_values), y_values)
+        a.scatter(x_values, y_values)
+
+        # a.invert_yaxis()
+
+        a.set_title(x +'vs.'+y , fontsize=12)
+        a.set_ylabel(y ,fontsize=12)
+        a.set_xlabel(x, fontsize=12)
+        a.grid()
+
+        canvas = FigureCanvasTkAgg(fig, master=self.Frame)
+
+        canvas.get_tk_widget().grid(column=0, row=1, columnspan=4, padx=20, pady=20)
+        canvas.draw()
+
+
+
+
+
+
+
+
+
+    def __init__(self, Frame, arraylist, int,gwidth, gheight):
+
+
+        '''ARRAY LIST WILL BE THE LIST OF DAYS DICTIONARY, FRAME WILL BE THE LOCATION OF THE BOARD'''
+        self.Frame = Frame
+
+        # WIDGETS
+        label = tkinter.Label(Frame, text = str(int) + '  Choose EX (Scrollable)')
+        graph_x = tkinter.Listbox(Frame, width =30, height = 1)
+        graph_y = tkinter.Listbox(Frame, width=30, height=1)
+        enter_button = tkinter.Button(Frame, text="ENTER", command = lambda: self.drawGraph(graph_x,graph_y))
+
+
+
+        # INSERTING ITEMS LISTBOXES
+        for i in range(0, len(arraylist)):
+            graph_x.insert(i, arraylist[i])
+
+        for i in range(0, len(arraylist)):
+            graph_y.insert(i, arraylist[i])
+
+        # PLACING WIDGETS ON SCREEN
+        label.grid(column=0, row = 0)
+        graph_x.grid(column=1, row=0)
+        graph_y.grid(column=2, row=0)
+        enter_button.grid(column=3, row=0)
+
+
+
+
+graph_number = ['1x1']
+
+exList = ['1','2','3','4','5','6']
+
+def save_graph_number(grid):
+    # CLEARS CONTENT
+    for widget in window.winfo_children():
+       widget.destroy()
+    # REDRAWS MENU
+    draw_menu()
+
+    graph_number[0] = grid
+    graph_array = graph_number[0].split('x')
+
+    for i in range(0,len(graph_array)):
+        graph_array[i] = int(graph_array[i])
+
+    # FINDS WIDTH AND HEIGHT OF GRAPHS
+    window.update_idletasks()
+    graph_width = window.winfo_width()/graph_array[0]-50
+    graph_height = window.winfo_height()/graph_array[1] - 50
+
+    for i in range(graph_array[0]):
+        for j in range(graph_array[1]):
+
+
+            frame = tkinter.Frame(window)
+            frame.grid(column=i, row=j)
+            board(frame,exList, i+1, graph_width,graph_height)
+
+
+
+def update_grid_size():
+    update_grid_window = tkinter.Toplevel(window)
+    update_grid_window.geometry('300x100')
+    # WIDGETS
+    label = tkinter.Label(update_grid_window, text="Enter Graph Grid (CxR): ")
+    enter_box = tkinter.Entry(update_grid_window)
+    enter_box.insert(0, graph_number[0])
+    enter_button = tkinter.Button(update_grid_window, text= "Enter", command = lambda:save_graph_number((enter_box.get().strip())))
+
+    # PUTS WIDGETS TO SCREEN
+    label.grid()
+    enter_box.grid(column=0,row = 1)
+    enter_button.grid(column=1, row=1)
+
+# MENU BAR NEEDS TO BE REDRAW WHEN DISPLAY IS REDRAWN
+def draw_menu():
+    menuBar = tkinter.Menu(window)
+    gridMenu = tkinter.Menu(menuBar)
+    menuBar.add_cascade(label='GRID', menu=gridMenu)
+    gridMenu.add_command(label='AMOUNT OF GRAPHS', command=update_grid_size)
+    window.config(menu=menuBar)
+
+# WINDOW SETTING
+window = tkinter.Tk()
+window.title("Program")
+window.geometry("800x600")
+
+draw_menu()
+
+
+
+# EXAMPLE LIST
+exList = list.ExList().copy()
+
+
+save_graph_number('1x1')
+
+
+window.mainloop()
+
+
+
+
+
+
+
+
+
---
+++ pro.py
@@ -0,0 +1,149 @@
+
+import matplotlib.dates as dates
+import matplotlib.pyplot as pyplot
+import tkinter
+
+FILE = 'ex2.csv'
+file = open(FILE,'r')
+
+
+
+
+# CREATED DICTIONARIES OF EACH DAY SO THAT EXCERCISES CAN BE ADDED
+DAYS = []
+
+# SAVES FIRST COLUMN
+INFO = []
+
+# SAVES ALL EXCERCISES
+EXLIST = ['DATES','TIME','WEIGHT']
+
+# FOLLOWING LINE CREATES AN ARRAY OF DATES
+DATES = file.readline().rstrip("\n").split(',')
+
+INFO.append(DATES[0])
+DATES.remove(DATES[0])
+CurrentEx = []
+for i in range (0,len(DATES)):
+    # CREATES A DICTIONARY WITH FIRST VALUE OF DATE
+    DAYS.append({'DATE':DATES[i]})
+
+    #CREATE A LIST OF THE TOTAL LENGHT OF DATA
+    CurrentEx.append('')
+i = 0
+
+
+for line in file:
+    # PREPARES LINE TO BE READABLE
+    DATA = line.rstrip('\n').split(',')
+
+    # SAVES THE  THE FIRST COLUMN
+    INFO.append(DATA[0])
+
+
+    if (DATA[0]) == 'TIME':
+
+        for j in range(1,len(DATA)):
+            DAYS[j-1].update({'TIME': DATA[j]})
+
+    if DATA[0] == 'WEIGHT':
+        for j in range(1,len(DATA)):
+            DAYS[j-1].update({'WEIGHT': DATA[j]})
+
+
+
+    if DATA[0] == 'EX':
+        # if CURRENTEX IS LESS THAN DATA
+        for j in range(1,len(DATA)):
+
+
+            # UPDATES CURRENT EX EVEN IF EMPTY
+            CurrentEx[j-1] = DATA[j]
+            if ((CurrentEx[j-1].strip() not in  EXLIST ) and (CurrentEx[j-1].strip() != '')):
+                EXLIST.append(CurrentEx[j-1].strip())
+
+            if DATA[j] != '':
+                DAYS[j-1].update({DATA[j]:[]})
+
+    if (DATA[0][0:3] == 'SET') or (DATA[0] == 'WU'):
+        for j in range(1,len(DATA)):
+            if DATA[j] != '':
+
+                DAYS[j-1][CurrentEx[j-1]].append(DATA[j])
+    i += 1
+
+
+# VOLUME IS USED TO AS A MEASUE TO CALCULATE PROGRESS
+def returnList (String, Volume = True ):
+    # IF DATES IS REQUESTED THEN RETURNS LIST USED IN PROGRAM
+    if String == 'DATES':
+        return DATES
+
+
+    # THIS IS USED FOR ALL OTHER REQUESTS
+    else:
+        # RETURN LIST
+        t = []
+        # LOOPS THOURGHOUT ALL DAYS IN ORDER TO RETRIEVE DATA
+        for i in range(0,len(DAYS)):
+
+            # IF THE DAY HAS THE REQUESTED EXRECISE
+            if String in DAYS[i]:
+                # VOLUME IS USED TO MEASURE PROGRESS OF AN EXRECISE, WILL BE THE DEFAULT, FOUND BY MULTIPYLING SETS BY REPS
+
+                if Volume:
+
+                    # FINDS THE VOLUME OF THE FIRST SET IF NOT EMPTY, APPENDS A VALUE IN ANY CASE SO THAT
+                    #  CAN BE USED WITH OTHER ARRAYS
+                    # DAYS[i] - PICKS A DAY, [String] - PICKS EXCERCISE IN DICTIONARY, [int] - SET OF AN EXCERCISE (A STRING)
+                    if DAYS[i][String] != '':
+
+                        # WE ARE ONLY FINDING THE VOLUME OF THE FIRST SET
+                        w = DAYS[i][String][1].split('/')
+
+                        # LOOPS THOUGH 2 THINGS
+                        for i in range(0, len(w)):
+
+                            w[i] = float(w[i])
+
+                        t.append(w[0] * w[1])
+
+                    # SAVES AN EMPTY SPACE IF SET IS EMPTY
+                    else:
+
+                        t.append(DAYS[i][String])
+                else:
+                    if String ==  'WEIGHT':
+                        if DAYS[i][String] != '':
+                            t.append(float(DAYS[i][String]))
+                        else:
+                            t.append(DAYS[i][String])
+
+            else :
+                t.append('')
+
+        return t
+
+
+# THIS METHOD WILL MAKE SURE LISTS HAVE SAME LENGHT
+def fixLenght(arrayone, arraytwo,number = 1):
+    # REMOVES DATE WHEN EXCERSICE IS NOT PRESENT
+    loopcounter = 0
+    while loopcounter < len(arrayone):
+
+        # REMOVES
+        if arraytwo[loopcounter] == '':
+            arraytwo.remove(arraytwo[loopcounter])
+            arrayone.remove(arrayone[loopcounter])
+            loopcounter -= 1
+        loopcounter += 1
+        if number == 1:
+            fixLenght(arraytwo,arrayone,number = 2)
+
+
+def ExList():
+    return EXLIST
+
+
+
+
